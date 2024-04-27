ddae5293225633b53143516e4af0a128f9905677
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2024-04-27 11:52:56 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import {SETTINGS} from "./settings"
 import { videosRouter } from './videos'
-
+//test
 export const app = express()
 app.use(express.json()) // добавление ко всем реквестам body и query
 
