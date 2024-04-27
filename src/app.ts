a01ac7cd5b71527f4260e1736346d80847af01e6
Akerke Tleugazy <akky.bauyrzhankyzy@gmail.com>
2024-04-27 11:42:35 +0600

    Added main route

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,11 @@ import { videosRouter } from './videos'
 
 export const app = express()
 app.use(express.json()) // добавление ко всем реквестам body и query
- 
+
+app.get('/', (req, res) => {
+    // эндпоинт, который будет показывать на верселе какая версия бэкэнда сейчас залита
+    res.status(200).json({version: '1.0'})
+})
+
 //app.get(SETTINGS.PATH.VIDEOS, getVideosController)
 app.use(SETTINGS.PATH.VIDEOS, videosRouter)
